9f1cb240ab10f59f8d9399063bd13077491de341
hyxklee <ewgt1234@naver.com>
2024-11-28 15:19:28 +0900

    feat: 최상위 커스텀 예외 추가

---
+++ src/main/java/goodpartner/be/global/auth/exception/BaseException.java
@@ -0,0 +1,14 @@
+package goodpartner.be.global.auth.exception;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseException extends RuntimeException {
+
+    private final int errorCode;
+
+    public BaseException(int errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
