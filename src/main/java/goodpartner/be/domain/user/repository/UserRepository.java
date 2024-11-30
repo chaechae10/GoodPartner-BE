2fcee8fca9c5780f684bc9562f6c5a05e126f30f
hyxklee <ewgt1234@naver.com>
2024-12-01 01:17:02 +0900

    feat: userId를 UUID로 수정

--- src/main/java/goodpartner/be/domain/user/repository/UserRepository.java
+++ src/main/java/goodpartner/be/domain/user/repository/UserRepository.java
@@ -4,8 +4,9 @@ import goodpartner.be.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByKakaoId(Long kakaoId);
 
     boolean existsByKakaoId(Long kakaoId);
